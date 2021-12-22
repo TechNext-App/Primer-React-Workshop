da85472dd2ac0ae55cad473e9fc6bfcb22838414
Giancarlo <ggarciadeleon@csustan.edu>
2021-12-21 22:03:01 -0800

    first commit, first iteration of markdown application

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 function App() {
+  const [input, setInput] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea className="textarea" value={input} 
+      onChange={(e) => setInput(e.target.value)}/>
+      <ReactMarkdown className="preview-markdown" children={input}/>
     </div>
   );
 }
-
 export default App;
