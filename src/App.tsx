e26caf3c5211add00923e7b834a5f9f0b16f7ad0
Giancarlo <ggarciadeleon@csustan.edu>
2021-12-24 18:32:52 -0800

    added syntax-highlighter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import remarkGfm from 'remark-gfm'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 function App() {
   const [input, setInput] = useState("");
@@ -8,7 +11,27 @@ function App() {
     <div className="App">
       <textarea className="textarea" value={input} 
       onChange={(e) => setInput(e.target.value)}/>
-      <ReactMarkdown className="preview-markdown" children={input}/>
+      <ReactMarkdown className="preview-markdown" 
+      children={input}
+      remarkPlugins={[[remarkGfm, {singleTilde: false}]]}
+      components={{
+        code({node, inline, className, children, ...props}) {
+          const match = /language-(\w+)/.exec(className || '')
+          return !inline && match ? (
+            <SyntaxHighlighter
+              children={String(children).replace(/\n$/, '')}
+              style={docco}
+              language={match[1]}
+              PreTag="div"
+            />
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          )
+        }
+      }}
+       />
     </div>
   );
 }
